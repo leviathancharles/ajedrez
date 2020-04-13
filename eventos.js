0341504247da52125b4436b92359048e0e433131
Carlos Hurtado <gabo_duty@hotmail.com>
2020-04-13 15:46:07 -0500

    tablero completo

--- eventos.js
+++ eventos.js
@@ -9,16 +9,56 @@ boton.addEventListener("click", crearTablero);
 function crearTablero() {
     let cantidad = document.getElementById("cantidad").value;
     if (cantidad > 5 ) {
+        
         const tablero = cantidad * cantidad;
+        const parImpar = cantidad % 2;
+        var color = "white";
+        var contador = 0;
+
         for (let i = 1; i <= tablero; i++) {
 
             let residuo = i % cantidad;
-                
+            let calculo = i%2;
+            let calculoColor = contador%2;
+            console.log("color: " +i+ " modulo color "+calculoColor);
+            console.log("calculo: " +i+ " calculo " +calculo);
+
+            if (parImpar == 0){
+                console.log("numero par");    
+            }else{
+                console.log("numero impar");
+            }
+            
+            
             var div = document.createElement("div");
             div.className = "caja";
+            
+            if(calculo ==  0 ){
+                if(calculoColor == 0){
+                   color = "black";
+                }else{
+                   color = "white";        
+                }    
+            }else{
+                if(calculoColor == 0){
+                    color = "white";
+                }else{
+                    
+                    color = "black";
+                }
+            }
+
+            div.style.cssText = 'background-color: '+ color +';'
+            
            
-            var newContent = document.createTextNode(i); 
-            div.appendChild(newContent);
+            
+            //var v = document.createElement("p");
+            //v.textContent = calculo;
+            //document.getElementById("valores").appendChild(v);
+            
+           
+            //var newContent = document.createTextNode(i); 
+            //div.appendChild(newContent);
 
             document.getElementById("contenedor").appendChild(div);
 
@@ -26,8 +66,13 @@ function crearTablero() {
             {
                 var salto = document.createElement("br");
                 document.getElementById("contenedor").appendChild(salto);
+                contador++;
 
             }
+
+        
+            
+         
                
         }
        
